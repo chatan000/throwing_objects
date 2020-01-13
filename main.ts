9543336cf8410651afc4f3f0381f03435f67eeb4
chatan000 <58610792+chatan000@users.noreply.github.com>
2020-01-13 13:54:53 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,24 @@
-function score () {
-    info.changeScoreBy(1)
+function hero () {
+    Hero = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . b e b . . . 
+. . . . . . . . . b e b . . . . 
+. . . . . . b b b b b b . . . . 
+. . . . . b b a a a a a b . . . 
+. b b b b b a a a a a a a b . . 
+. b 6 8 b a a a a a a a a b . . 
+. . b 8 8 b a 6 1 f a d 4 f . . 
+. . b 6 8 8 b 1 f f a 4 4 c . . 
+b b 6 b 8 8 7 6 f b 4 4 4 4 4 b 
+b 6 6 c 6 7 7 b 7 4 4 4 4 4 b . 
+c 6 6 6 c c b 7 7 7 7 7 7 7 b . 
+c b 6 6 6 6 6 7 7 7 7 7 7 7 b . 
+. c 6 6 6 6 6 6 7 7 7 7 7 6 b . 
+. . c b 6 6 6 6 6 7 7 7 b b . . 
+. . . c c c c c c c c b b . . . 
+`, SpriteKind.Player)
+    controller.moveSprite(Hero)
+    Hero.setPosition(126, 89)
 }
 function cloud () {
     cloud1 = sprites.createProjectileFromSide(img`
@@ -29,6 +48,12 @@ b b b 1 1 1 1 1 1 1 1 1 1 1 b b b 1 1 1 1 1 1 1 1 1 1 1 1 1 b 1 1 1 1 1 1 1 1 1 
 . . . . . . . . . . . . . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . . . . . . . . . . . . 
 `, 50, 100)
 }
+function score () {
+    info.changeScoreBy(1)
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    ball()
+})
 function ball () {
     projectile = sprites.createProjectileFromSprite(img`
 . . . . . . b b b b b b b b . . 
@@ -36,9 +61,9 @@ function ball () {
 . . . . . b b b b b b b b c . . 
 . . . . c b b b b b b b b c . . 
 . . . c b b b b b b b b b c c . 
-. . . c b b b b b b b c c c c . 
-. . c c b b b b c c c c c c c . 
-. . c c c b b b b b c d d d c c 
+. . . c d b b b b b b c c c c . 
+. . c c d b b b c c c c c c c . 
+. . c c c d d b b b c d d d c c 
 . c b b c c b b c c d d d d d c 
 . c b b b b b b c c d d d d d c 
 . c c b b b b c b c b d d d b c 
@@ -49,30 +74,6 @@ c c c c c c c b b b b b c c c c
 c c c c c c c c b b b b b c c c 
 `, Hero, 0, -50)
 }
-function hero () {
-    Hero = sprites.create(img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . b e b . . . 
-. . . . . . . . . b e b . . . . 
-. . . . . . b b b b b b . . . . 
-. . . . . b b a a a a a b . . . 
-. b b b b b a a a a a a a b . . 
-. b 6 8 b a a a a a a a a b . . 
-. . b 8 8 b a 6 1 f a d 4 f . . 
-. . b 6 8 8 b 1 f f a 4 4 c . . 
-b b 6 b 8 8 7 6 f b 4 4 4 4 4 b 
-b 6 6 c 6 7 7 b 7 4 4 4 4 4 b . 
-c 6 6 6 c c b 7 7 7 7 7 7 7 b . 
-c b 6 6 6 6 6 7 7 7 7 7 7 7 b . 
-. c 6 6 6 6 6 6 7 7 7 7 7 6 b . 
-. . c b 6 6 6 6 6 7 7 7 b b . . 
-. . . c c c c c c c c b b . . . 
-`, SpriteKind.Player)
-    Hero.setPosition(126, 89)
-}
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    ball()
-})
 function rain2 () {
     rain = sprites.createProjectileFromSprite(img`
 . . . . . . . . . . . . . . . . 
@@ -96,9 +97,9 @@ function rain2 () {
     rain.x += Math.randomRange(0, 10)
 }
 let rain: Sprite = null
-let Hero: Sprite = null
 let projectile: Sprite = null
 let cloud1: Sprite = null
+let Hero: Sprite = null
 cloud()
 hero()
 ball()
